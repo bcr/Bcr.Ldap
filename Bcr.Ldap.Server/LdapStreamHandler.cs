a7eadf427d147e2c12f724261626a5057d5e3170
Blake Ramsdell <blaker@gmail.com>
2024-04-30 02:15:12 -0700

    Add LdapResult

--- Bcr.Ldap.Server/LdapStreamHandler.cs
+++ Bcr.Ldap.Server/LdapStreamHandler.cs
@@ -37,49 +37,6 @@ class LdapStreamHandler : IStreamHandler
         SASL = 0xA3,
     }
 
-    enum LdapResultCode
-    {
-        Success = 0,
-        OperationsError = 1,
-        ProtocolError = 2,
-        TimeLimitExceeded = 3,
-        SizeLimitExceeded = 4,
-        CompareFalse = 5,
-        CompareTrue = 6,
-        AuthMethodNotSupported = 7,
-        StrongerAuthRequired = 8,
-        Referral = 10,
-        AdminLimitExceeded = 11,
-        UnavailableCriticalExtension = 12,
-        ConfidentialityRequired = 13,
-        SaslBindInProgress = 14,
-        NoSuchAttribute = 16,
-        UndefinedAttributeType = 17,
-        InappropriateMatching = 18,
-        ConstraintViolation = 19,
-        AttributeOrValueExists = 20,
-        InvalidAttributeSyntax = 21,
-        NoSuchObject = 32,
-        AliasProblem = 33,
-        InvalidDNSyntax = 34,
-        AliasDereferencingProblem = 36,
-        InappropriateAuthentication = 48,
-        InvalidCredentials = 49,
-        InsufficientAccessRights = 50,
-        Busy = 51,
-        Unavailable = 52,
-        UnwillingToPerform = 53,
-        LoopDetect = 54,
-        NamingViolation = 64,
-        ObjectClassViolation = 65,
-        NotAllowedOnNonLeaf = 66,
-        NotAllowedOnRDN = 67,
-        EntryAlreadyExists = 68,
-        ObjectClassModsProhibited = 69,
-        AffectsMultipleDSAs = 71,
-        Other = 80,
-    }
-
     private readonly ILogger<LdapStreamHandler> _logger;
 
     public LdapStreamHandler(ILogger<LdapStreamHandler> logger)
